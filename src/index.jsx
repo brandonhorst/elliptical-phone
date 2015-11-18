fb387de2dcfea9b0b8ed441287951f843e09c91f
Brandon Horst <brandonhorst@gmail.com>
2015-11-18 14:06:00 -0500

    Update deps

--- src/index.jsx
+++ src/index.jsx
@@ -4,22 +4,24 @@ import {createElement, Phrase} from 'lacona-phrase'
 export default class PhoneNumber extends Phrase {
 
   getValue (result) {
+    if (!result) return
+
     return result.replace(/[ ()/-]/g, '')
   }
 
   displayWhen (input) {
-    return /^\+?\(?(\d[ ()/-]{0,2}){0,5}\d?$/.test(input)
+    return /^\+?\(?(\d[ ()/-]{0,2}){0,6}$/.test(input)
   }
 
   validate (input) {
-    return /^\+?\(?(\d[ ()/-]{0,2}){6,14}\d$/.test(input)
+    return /^\+?\(?(\d[ ()/-]{0,2}){7,15}$/.test(input)
   }
 
   describe() {
     return (
-      <placeholder descriptor='phone number' displayWhen={this.displayWhen}>
+      <argument text='phone number' displayWhen={this.displayWhen}>
         <freetext validate={this.validate} splitOn={/\w/} />
-      </placeholder>
+      </argument>
     )
   }
 }
